fcf72f7b96cbd3f470c6bda39fadc359769b9869
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-02 23:32:57 -0600

    routes added

---
+++ src/routes/routes.tsx
@@ -0,0 +1,27 @@
+import { Route, Outlet } from 'react-router-dom';
+import { Fragment, Suspense } from 'react';
+import { RoutePropsI } from '../interfaces/Routes';
+
+export const RenderRoutes = (routes: RoutePropsI[]) => {
+    return routes.map((route, index) => {
+        const Component = route.element || Fragment;
+        const Layout = route.layout || Fragment;
+        const Guard = route.guard || Fragment;
+        return <Route
+            key={index}
+            path={route.path}
+            element={
+                <Suspense fallback={<><h1>Loading...</h1></>}>
+                    <Guard>
+                        <Layout>
+                            {route.children ? <Outlet /> : <Component />}
+                        </Layout>
+                    </Guard>
+                </Suspense>
+            }
+        >
+            {route.children && RenderRoutes(route.children)}
+        </Route>
+    });
+}
+

5b6ee6a5bcb244fc98202b5114c16e493a1be998
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-02 23:56:04 -0600

    Updated Navbar and AuthToken

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -62,41 +62,6 @@ export const Navigation = () => {
                             ))}
                         </ul>
                         <hr />
-                        <div className="dropdown">
-                            <a
-                                href="#"
-                                className="d-flex align-items-center justify-content-evenly text-decoration-none dropdown-toggle"
-                                id="dropdownUser1"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                <i style={{ fontSize: '32px' }} className="bi bi-person-circle"></i>
-                                <strong>USER NAME</strong>
-                                {/* <strong>{this.userData.Alias}</strong> */}
-                            </a>
-                            <ul className="dropdown-menu shadow" id="options" aria-labelledby="dropdownUser1">
-                                <li>
-                                    Nueva Tarea
-                                    {/* <Link to="/Components/tareas" className="dropdown-item">
-                                        Nueva Tarea
-                                    </Link> */}
-                                </li>
-                                <li>
-                                    Configuración
-                                    {/* <Link to="/Components/configuracion" className="dropdown-item">
-                                        Configuración
-                                    </Link> */}
-                                </li>
-                                <li>
-                                    <hr className="dropdown-divider" />
-                                </li>
-                                <li>
-                                    <a href="#" className="dropdown-item" onClick={logOut}>
-                                        Cerrar Sesión
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
 
@@ -136,56 +101,13 @@ export const Navigation = () => {
                             <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close" onClick={handleOffcanvasClose}></button>
                         </div>
                         <div className="offcanvas-body">
-                            <ul className="navbar-nav fs-5 justify-content-end flex-grow-1">
-                                <li className="nav-item" data-bs-dismiss="offcanvas" >
-                                    Home
-                                    {/* <Link to="/Components/tareas-pendientes" className="nav-link" aria-current="page">
-                                        <i className="bi bi-house"></i>
-                                        Home
-                                    </Link> */}
-                                </li>
-                                <li data-bs-dismiss="offcanvas" className="nav-link">
-                                    Tareas Pendientes
-                                    {/* <Link to="/Components/tareas-pendientes" className="nav-link">
-                                        <i className="bi bi-list-ol"></i>
-                                        Tareas Pendientes
-                                    </Link> */}
-                                </li>
-                                <li data-bs-dismiss="offcanvas" className="nav-link">
-                                    Tareas Completadas
-                                    {/* <Link to="/Components/tareas-completadas" className="nav-link">
-                                        <i className="bi bi-list-check"></i>
-                                        Tareas Completadas
-                                    </Link> */}
-                                </li>
-                                <li data-bs-dismiss="offcanvas" className="nav-link">
-                                    Frases Guardadas
-                                    {/* <Link to="/Components/frases-guardadas" className="nav-link">
-                                        <i className="bi bi-folder"></i>
-                                        Frases Guardadas
-                                    </Link> */}
-                                </li>
-                                <li data-bs-dismiss="offcanvas" className="nav-link" >
-                                    Configuración
-                                    {/* <Link to="/Components/configuracion" className="nav-link">
-                                        <i className="bi bi-gear"></i>
-                                        Configuración
-                                    </Link> */}
-                                </li>
-                                <li>
-                                    <a href="#" className="nav-link" onClick={logOut}>
-                                        <i className="bi bi-box-arrow-right"></i>
-                                        Cerrar Sesión
-                                    </a>
-                                </li>
+                            <ul className="nav nav-pills flex-column mb-auto">
+                                {routes.map((route, index) => (
+                                    <li key={index}>
+                                        <Link to={route.path} className="nav-link" aria-current="page" >{route.name}</Link>
+                                    </li>
+                                ))}
                             </ul>
-                            <div className="text-white">
-                                <div className="d-flex align-items-center justify-content-center text-decoration-none mt-5" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i style={{ fontSize: '25px' }} className="bi bi-person-circle"></i>
-                                    <strong className="ms-2">USER NAME</strong>
-                                    {/* <strong className="ms-2">{this.userData.Alias}</strong> */}
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
--- src/guards/AuthToken.tsx
+++ src/guards/AuthToken.tsx
@@ -1,11 +1,12 @@
 import { Navigate } from 'react-router-dom';
 import { PrincipalPropsI } from "../interfaces/Principal"
+import { PATHS } from '../const/Paths';
 
 const AuthToken = ({ children }: PrincipalPropsI) => {
-    
+
     const token = sessionStorage.getItem('ACCESS_TOKEN') || null;
     if (!token) {
-        return <Navigate to='/login' />
+        return <Navigate to={PATHS.LOGIN} />
     }
 
     return (

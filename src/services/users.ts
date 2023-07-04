e827b97f2952a5962261b08ef0e34b9def2c065a
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-04 00:52:15 -0600

    logOut and getToken implemeted

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,6 +1,18 @@
 import { SaveTokenI } from "../interfaces/User";
 
-export const SaveToken = ({ accessToken, expiresIn }: SaveTokenI) => {
+export const SaveToken = ({ accessToken, expiresIn, name }: SaveTokenI) => {
     sessionStorage.setItem('ACCESS_TOKEN', accessToken);
     sessionStorage.setItem('EXPIRES_IN', expiresIn);
+    sessionStorage.setItem('USER', name);
+}
+
+export const logOut = () => {
+    sessionStorage.removeItem('ACCESS_TOKEN');
+    sessionStorage.removeItem('EXPIRES_IN');
+    sessionStorage.removeItem('USER');
+}
+
+export const getToken = () => {
+    const token = sessionStorage.getItem('ACCESS_TOKEN') || '';
+    return token;
 }

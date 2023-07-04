0f5ddc78994adbf4fde2d7c295f9acb4c0389ec1
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-03 21:52:11 -0600

    response auth user created

--- src/interfaces/User.tsx
+++ src/interfaces/User.tsx
@@ -2,3 +2,15 @@ export interface LoginI {
     UserName: string,
     Password: string
 }
+
+export interface SaveTokenI {
+    accessToken: string,
+    expiresIn: string,
+    name: string,
+    email: string
+}
+
+export interface ResponseAuthUserI {
+    status: string,
+    data: SaveTokenI
+}

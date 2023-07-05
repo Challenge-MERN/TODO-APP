d51bcc5e9d113a1afde02d1dd917ee1433c3329c
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-04 19:57:10 -0600

    TaskGrid component implemented

--- src/components/tasks/TaskGrid.tsx
+++ src/components/tasks/TaskGrid.tsx
@@ -1,18 +1,27 @@
+import { Link } from 'react-router-dom';
 import { useFetchPendingTasks } from '../../hooks/useFetchPendingTasks';
 import { PendingTasksI } from '../../interfaces/Tasks';
 import { TaskItem } from './TaskItem';
 
 export const TaskGrid = () => {
     const User = sessionStorage.getItem('USER') || '';
-    const { data, message } = useFetchPendingTasks(User);
+    let hiddenLink = true;
+    const letsGo = '>>>';
 
-    console.log(data, message);
+    const { dataResponse, isLoading } = useFetchPendingTasks(User);
+
+    dataResponse.data.length === 0 ? hiddenLink = false : hiddenLink = true;
+
+    console.log(dataResponse.data, dataResponse.message);
 
     return (
         <>
-            <h1>{message}</h1>
+            <h2 className='mt-4'>{dataResponse.message}</h2>
             {
-                data.map((task: PendingTasksI) => {
+                !isLoading && (<Link className='mt-5 btn btn-outline-primary' hidden={hiddenLink} to={'/create-task'}>Crear Tarea {letsGo}</Link>)
+            }
+            {
+                dataResponse.data.map((task: PendingTasksI) => {
                     <TaskItem key={task._id} task={task} />
                 })
             }

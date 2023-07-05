575bc39fa5843adc15bcba17c9a0b87069554ad0
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-04 23:46:57 -0600

    AuthToken guard updated

--- src/guards/AuthToken.tsx
+++ src/guards/AuthToken.tsx
@@ -1,13 +1,32 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { PrincipalPropsI } from "../interfaces/Principal"
 import { PATHS } from '../const/Paths';
+import { getUserName, logOut } from '../services/users';
+import { getPendingTasks } from '../helpers/getPendingTasks';
+import { useEffect } from 'react';
 
 const AuthToken = ({ children }: PrincipalPropsI) => {
+    const navigate = useNavigate();
+    const userName = getUserName();
 
     const token = sessionStorage.getItem('ACCESS_TOKEN') || null;
-    if (!token) {
-        return <Navigate to={PATHS.LOGIN} />
+
+    const validToken = async () => {
+        if (!token) {
+            return <Navigate to={PATHS.LOGIN} />
+        } else {
+            try {
+                getPendingTasks(userName);
+            } catch (err) {
+                logOut();
+                navigate('/');
+            }
+        }
     }
+
+    useEffect(() => {
+        validToken();
+    }, [])
 
     return (
         <>

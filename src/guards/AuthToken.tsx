33e008ca067071940d624d2a664325d2006b0ac2
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-05 22:31:47 -0600

    Added alert notification on Guard

--- src/guards/AuthToken.tsx
+++ src/guards/AuthToken.tsx
@@ -4,22 +4,38 @@ import { PATHS } from '../const/Paths';
 import { getUserName, logOut } from '../services/users';
 import { getPendingTasks } from '../helpers/getPendingTasks';
 import { useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 const AuthToken = ({ children }: PrincipalPropsI) => {
     const navigate = useNavigate();
     const userName = getUserName();
 
     const token = sessionStorage.getItem('ACCESS_TOKEN') || null;
-
+    console.log('estou entradnosd asd alk lsak jfas fasfl ks hflkjs')
     const validToken = async () => {
         if (!token) {
-            return <Navigate to={PATHS.LOGIN} />
+            navigate('/');
         } else {
             try {
-                getPendingTasks(userName);
+                await getPendingTasks(userName);
             } catch (err) {
-                logOut();
-                navigate('/');
+                Swal.fire({
+                    icon: 'info',
+                    title: 'Sesión expirada!',
+                    text: 'Es necesario volver a iniciar sesión.',
+                    allowEnterKey: false,
+                    allowEscapeKey: false,
+                    allowOutsideClick: false,
+                    showCancelButton: false,
+                    showConfirmButton: true,
+                    confirmButtonColor: 'blue',
+                    confirmButtonText: 'Iniciar Sesión'
+                }).then((res) => {
+                    if (res.isConfirmed) {
+                        logOut();
+                        navigate('/');
+                    }
+                });
             }
         }
     }

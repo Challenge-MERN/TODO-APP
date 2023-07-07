8469994fc14935bbafba8efbbfbc8771718e9925
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-07-06 18:07:05 -0600

    Appname component created

---
+++ src/components/AppName.tsx
@@ -0,0 +1,11 @@
+
+export const AppName = () => {
+    const appName = 'TODO | APP';
+
+    return (
+        <>
+            <h2 style={{ fontFamily: 'serif', fontWeight: 'bold' }}>{appName}</h2>
+        </>
+    )
+}
+
